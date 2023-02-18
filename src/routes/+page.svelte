a24db863ab4ff28d5b2dd56000ca35f658ca79cc
Иван Григ <abstractgrig@gmail.com>
2023-02-18 15:32:30 +0300

    added tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,20 @@
-<h1>Welcome to Golden Ratio ProtoPage test 3</h1>
+<script>
+    import logo from "$lib/assets/gr-logo.png";
+</script>
+
+<div class="flex items-center justify-between">
+    <div class="flex-none m-2">
+        <div class="w-7 bg-black h-1 rounded" />
+        <div class="my-1 w-7 bg-black h-1 rounded" />
+        <div class="w-7 bg-black h-1 rounded" />
+    </div>
+
+    <div class="text-center tracking-widest">
+        <span class="text-xs">
+        КЛИНИКА СТОМАТОЛОГИ
+        </span>
+        <p class="bg-accent">ЗОЛОТОЕ СЕЧЕНИЕ</p>
+    </div>
+</div>
+
+<img src={logo} alt="download icon" />
---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js,svelte,ts}"],
+    theme: {
+        extend: {
+            colors: {
+                accent: "#FFD700",
+                black: "#44403C"
+            },
+        },
+    },
+    plugins: [],
+};
